38514c0a136cc95a9fc42ac4d6127b9011ca49c7
zivbabs <117026503+zivbabs@users.noreply.github.com>
2024-02-09 10:53:48 +0100

    final solution handin for week2

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 "use-strict"
 
-let GUESS = 0;
+let GUESS = null;
+let HIGH = null;
+let LOW = 1;
 
 window.addEventListener("DOMContentLoaded", start);
 
 function start() {
-    const startButton = document.querySelector("#start");
+    const startButton = document.querySelector("#start-game");
     const actions = document.querySelector("#actions");
+    const inputValue = document.querySelector("#high-value");
     startButton.addEventListener("click", function () {
+        HIGH = parseInt(inputValue.value);
         guessNumber();
         startButton.style.display = "none";
         actions.style.display = "block";
+        inputValue.style.display = "none";
     });
 }
 function guessNumber(){
-    GUESS = Math.floor(Math.random() * 99) + 1;
-    const list = document.querySelector("#guesses");
+    GUESS = Math.floor((HIGH + LOW) / 2);
+    const list = document.querySelector("#guess-list");
     list.insertAdjacentHTML("beforeend", `<li>${list.childElementCount+1}. My GUESS is: ${GUESS}</li>`);
     userResponse(GUESS);
 }
@@ -26,30 +31,32 @@ function userResponse(){
 }
 function handleUserResponse(event){
     event.preventDefault();
-    if(event.target.id === "low-btn"){
+    if(event.target.id === "too-low-btn"){
         tooLow();
     }
-    else if(event.target.id === "high-btn"){
+    else if(event.target.id === "too-high-btn"){
         tooHigh();
     }
-    else if(event.target.id === "correct-btn"){
+    else if(event.target.id === "success-btn"){
         correct();
     }
 }
 function tooLow(){
-    const list = document.querySelector("#guesses");
+    LOW = GUESS + 1;
+    const list = document.querySelector("#guess-list");
     const lastElement = list.lastElementChild;
     lastElement.innerHTML += " - too low";
     guessNumber();
 }
 function tooHigh(){
-    const list = document.querySelector("#guesses");
+    HIGH = GUESS - 1;
+    const list = document.querySelector("#guess-list");
     const lastElement = list.lastElementChild;
     lastElement.innerHTML += " - too high";
     guessNumber();
 }
 function correct(){
-    const list = document.querySelector("#guesses");
+    const list = document.querySelector("#guess-list");
     if(list.childElementCount === 1){
         list.insertAdjacentHTML("beforeend", `<li>Success! It took me ${list.childElementCount} GUESS.</li>`);
     } else {
